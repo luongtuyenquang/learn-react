8b5790cd9c96d105cc5cd88c5b3d3bfdcc9b40b3
TuyenQuang <luongtuyenquang77@gmail.com>
2021-09-23 23:05:51 +0700

    C2-Props & Exercise Courses

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import './App.css';
 import Header from './components/Header';
 import Product from './components/Product';
 
+const courses = [
+    {
+        id: 1,
+        name: 'HTML-CSS',
+        price: 10,
+    },
+    {
+        id: 2,
+        name: 'Javascript',
+        price: 20,
+    },
+    {
+        id: 3,
+        name: 'PHP',
+        price: 30,
+    },
+    {
+        id: 4,
+        name: 'Python',
+        price: 0,
+    },
+]
+
+const showCourse = courses.map((course) => {
+    if(course.price > 0){
+        return <Product name={course.name} price={course.price} key={course.id}/>
+    }
+})
+
 function App() {
     return <div className='wrapper'>
         <Header />
-        <Product />
+        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+        {showCourse}
+        </div>
     </div>;
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,48 +1,17 @@
+import React from "react";
 
-function Product() {
+function Product(props) {
     return (
-       <div className='row'>
-           <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                <div class="thumbnail">
-                        <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="..." />
-                        <div class="caption">
-                            <h3>Iphone 13 Pro Max</h3>
-                            <p>Giá: 20.000.000</p>
-                            <a href="#" class="btn btn-primary" role="button">Mua</a>
-                        </div>
-                </div>
+        <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
+            <div className="thumbnail">
+                    <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="" />
+                    <div className="caption">
+                        <h3>{props.name}</h3>
+                        <p>{props.price}</p>
+                        <a href="#1" className="btn btn-primary" role="button">Mua</a>
+                    </div>
             </div>
-           <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                <div class="thumbnail">
-                    <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="Giá: 20.000.000" />
-                        <div class="caption">
-                            <h3>Iphone 13 Pro Max</h3>
-                            <p>...</p>
-                            <a href="#" class="btn btn-primary" role="button">Mua</a>
-                        </div>
-                </div>
-            </div>
-           <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                <div class="thumbnail">
-                    <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="Giá: 20.000.000" />
-                        <div class="caption">
-                            <h3>Iphone 13 Pro Max</h3>
-                            <p>...</p>
-                            <a href="#" class="btn btn-primary" role="button">Mua</a>
-                        </div>
-                </div>
-            </div>
-           <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-                <div class="thumbnail">
-                    <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="Giá: 20.000.000" />
-                        <div class="caption">
-                            <h3>Iphone 13 Pro Max</h3>
-                            <p>...</p>
-                            <a href="#" class="btn btn-primary" role="button">Mua</a>
-                        </div>
-                </div>
-            </div>
-       </div>
+        </div>
     );
 }
 
