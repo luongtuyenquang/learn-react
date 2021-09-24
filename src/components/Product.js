719a061ac8c98b51199e6359ccc233401f172b4a
TuyenQuang <luongtuyenquang77@gmail.com>
2021-09-24 20:31:28 +0700

    C3-Handling Events

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Product(props) {
+    const showLog = () => {
+        console.log(props.name)
+    }
     return (
         <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
             <div className="thumbnail">
@@ -8,9 +11,9 @@ function Product(props) {
                     <div className="caption">
                         <h3>{props.name}</h3>
                         <p>{props.price}</p>
-                        <a href="#1" className="btn btn-primary" role="button">Mua</a>
+                        <a href="#1" className="btn btn-primary" onClick={ showLog }>Mua</a>
                     </div>
-            </div>
+            </div>role="button"
         </div>
     );
 }
