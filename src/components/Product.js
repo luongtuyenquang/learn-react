fde51e6e0929c6e502b32aa3e1f5d4b1a6ecc142
TuyenQuang <luongtuyenquang77@gmail.com>
2021-09-25 12:59:06 +0700

    C4-State

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,51 @@
 import React from "react";
+import { Component } from "react";
 
-function Product(props) {
-    const showLog = () => {
-        console.log(props.name)
+class Product extends Component {
+    constructor(){
+        super()
+        this.state = {
+            courses: [
+                {
+                    id: 1,
+                    name: 'HTML-CSS',
+                    price: 10,
+                },
+                {
+                    id: 2,
+                    name: 'Javascript',
+                    price: 20,
+                },
+                {
+                    id: 3,
+                    name: 'PHP',
+                    price: 30,
+                },
+                {
+                    id: 4,
+                    name: 'Python',
+                    price: 0,
+                },
+            ]
+        }
     }
-    return (
-        <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3'>
-            <div className="thumbnail">
-                    <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="" />
-                    <div className="caption">
-                        <h3>{props.name}</h3>
-                        <p>{props.price}</p>
-                        <a href="#1" className="btn btn-primary" onClick={ showLog }>Mua</a>
-                    </div>
-            </div>role="button"
-        </div>
-    );
+    render(){
+        const showCourse = this.state.courses.map((course) => {
+            if(course.price > 0){
+                return <div className='col-xs-3 col-sm-3 col-md-3 col-lg-3' key={course.id}>
+                            <div className="thumbnail">
+                                <img className='img' src="https://cdn.tgdd.vn/Products/Images/42/223602/iphone-13-midnight-2-600x600.jpg" alt="" />
+                                <div className="caption">
+                                    <h3>{course.name}</h3>
+                                    <p>{course.price}</p>
+                                    <a href="#1" className="btn btn-primary" role="button">Mua</a>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                })
+        return showCourse
+    }
 }
 
 export default Product;
