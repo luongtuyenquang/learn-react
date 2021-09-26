008786894865cb814a64b082e95800e45a63a627
TuyenQuang <luongtuyenquang77@gmail.com>
2021-09-26 19:45:06 +0700

    C6-1 - Form - get data from input=text

---
+++ src/Form.js
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Component } from 'react';
+
+
+class Form extends Component {
+    constructor(){
+        super()
+        this.state = {
+            inputUsername: '',
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            inputUsername: e.target.value
+        })
+    }
+    handleSubmit =(e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+    render(){
+        return <div className='wrapper'><br /><br />
+                    <div className='container'>
+                        <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6'>
+                            <div className="panel panel-default">
+                                <div className="panel-heading">Form</div>
+                                <div className="panel-body">
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group">
+                                        <label>User name:</label>
+                                        <input type="text"
+                                            className="form-control" 
+                                            name="inputUsername"
+                                            onChange={this.handleChange}
+                                        /><br />  
+                                        <button type="submit" className="btn btn-primary">Lưu lại</button>
+                                    </div>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    }
+    
+}
+
+export default Form;
