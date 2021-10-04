dbb88ddf2733069a64c42baaabe9007a37b2c8e2
TuyenQuang <luongtuyenquang77@gmail.com>
2021-10-04 16:12:12 +0700

    C9-Conditional Rendering

--- src/Test.js
+++ src/Test.js
@@ -7,38 +7,21 @@ class Test extends Component {
     constructor(){
         super()
         this.state = {
-            count: 0,
-            isShow: false
+            isShow: true
         }
     }
-    handleCrease = () => {
+    handleClick = () => {
         this.setState({
-            count: this.state.count + 1
+            isShow: !this.state.isShow
         })
-    }
-    handleDecrease = () => {
-        this.setState({
-            count: this.state.count - 1
-        })
-    }
-
-    componentDidMount(){
-        console.log('ComponentDidMount');
-    }
-    componentDidUpdate(){
-        console.log('ComponentDidUpdate');
-    }
-    componentWillUnmount(){
-        console.log('ComponentWillUnmount');
     }
     render(){
-        console.log('Render');
+        const show = this.state.isShow
         return (
             <div>
-                <p onClick={this.handleDecrease}>-</p>
-                <span>{this.state.count}</span>
-                <p onClick={this.handleCrease}>+</p>
-                <p>{this.state.isShow ? 'Delete' : ''}</p>
+                <button onClick={this.handleClick}>{show ? 'Hiện' : 'Ẩn'}</button>
+                <p>{show ? '' : 'Hello World'}</p>
+                {show ? <div>Hiện ra (show = true)</div> : <div>Hiện ra (show = false)</div>}
             </div>
         )
     }
