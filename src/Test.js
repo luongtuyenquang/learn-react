8ec89da7353084fe834dccfefa2778dc20fd635b
TuyenQuang <luongtuyenquang77@gmail.com>
2021-10-07 08:31:46 +0700

    C12-Hooks - useEffect-Pagination

--- src/Test.js
+++ src/Test.js
@@ -2,21 +2,44 @@
 // import './App.scss';
 import { useEffect, useState } from 'react';
 import PostList from './hooks/PostList';
+import Pagination from './hooks/Pagination';
+import queryString from 'query-string'
 
 function Test(){
     const [postList, setPostList] = useState([])
+    const [pagination, setPagination] = useState({
+        _page: 1,
+        _limit: 10,
+        _totalRow: 50
+    })
+    const [filter, setFilter] = useState({
+        _limit: 10,
+        _page: 1
+    })
     useEffect(() => {
         async function fetchAPI(){
-            const url = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1'
+            const paramString = queryString.stringify(filter)
+            const url = `http://js-post-api.herokuapp.com/api/posts?${paramString}`
             const responsive = await fetch(url)
             const responsiveJSON =  await responsive.json()
             setPostList(responsiveJSON.data)
+            setPagination(responsiveJSON.pagination)
         }
         fetchAPI()
-    }, [])
+    }, [filter])
+    function handlePageChange(newPage){
+        setFilter({
+            ...filter,
+            _page: newPage
+        })
+    }
     return (
         <div>
             <PostList posts={postList}/>
+            <Pagination 
+                pagination={pagination}
+                pageChange={handlePageChange}
+            />
         </div>
     )
 }
