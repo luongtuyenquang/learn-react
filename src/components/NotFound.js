9dd95196cbc79557d8a59f8c380d1cd75857e1e3
TuyenQuang <luongtuyenquang77@gmail.com>
2021-10-17 19:27:40 +0700

    C16 - Router - History-goBack()

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 
 function NotFound(){
-    
+    const history = useHistory()
+
+    //goBack: quay trở lại trang trước đó
     return (
-        <h3>404 - This Page is Not Found</h3>
+        <div className='container'>
+            <h3>404 - This Page is Not Found</h3>
+            <button onClick={() => history.goBack()}>Back</button>
+        </div>
     )
 }
 export default NotFound
