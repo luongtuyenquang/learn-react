607314b74846b1363c20f89eae92778e473cea2b
TuyenQuang <luongtuyenquang77@gmail.com>
2021-11-05 21:12:32 +0700

    C17 - Hooks - useState - Two-way binding -  Example: type Radio

--- src/Hooks/useState/UseState.js
+++ src/Hooks/useState/UseState.js
@@ -2,27 +2,41 @@ import { useState } from "react"
 
 export default function UseState(){
 
-    // Ví dụ: click để nhận tiền ngẫu nhiên
-   const gifts = [
-       '10.000 VNĐ',
-       '20.000 VNĐ',
-       '50.000 VNĐ',
-       '100.000 VNĐ',
-       '200.000 VNĐ',
-       '500.000 VNĐ',
-   ]
+    // Two-way binding and Example: Radio
+    const courses = [
+        {
+            id: 1,
+            name: 'HTML'
+        },
+        {
+            id: 2,
+            name: 'CSS'
+        },
+        {
+            id: 3,
+            name: 'Javascript'
+        },
+    ]
+    const [checked, setChecked] = useState('')
 
-   const [gift, setGift] = useState()
-
-   function handleClick(){
-        const index = Math.floor(Math.random() * gifts.length)
-        setGift(gifts[index])
-   }
-
+    function handleSubmit(){
+        console.log(checked);
+    }
+   
     return (
         <div className='App'>
-            <h3>{gift ? gift : 'Vui lòng click vào nhận quà'}</h3>
-            <button onClick={handleClick}>Nhận quà</button>
+            <div style={{display: "grid"}}>
+                {
+                    courses.map(course => {
+                        return (
+                            <label key={course.id}>
+                                <input type='radio' checked={checked === course.id} onChange={() => setChecked(course.id)} /> {course.name}
+                            </label>
+                        )
+                    })
+                }
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 } 
