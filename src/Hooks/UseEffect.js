0d6b0c6b8e5a693bcfed2ce0763bddd376aa8e88
TuyenQuang <luongtuyenquang77@gmail.com>
2021-11-06 18:00:40 +0700

    C17 - Hooks - useEffect - Example: Change Tabs

--- src/Hooks/UseEffect.js
+++ src/Hooks/UseEffect.js
@@ -2,16 +2,31 @@ import { useEffect, useState } from "react"
 
 export default function UseEffect(){
 
-    // useEffect: Thay đổi thanh tiêu đề trên trang web khi nhập dữ liệu ở input
-   const [title, setTitle] = useState('')
-    
+    // useEffect: Nhấn vào tab nào thì sẽ hiện thông tin của tab đó
+   const tabs = ['photos', 'todos', 'users']
+   const [photos, setPhotos] = useState([])
+   const [type, setType] = useState('photos')
+
    useEffect(() => {
-        document.title = title
-   })
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(res => res.json())
+            .then(data => setPhotos(data))
+   }, [type])
 
     return (
         <div className='App'>
-            <input type='text' placeholder='Nhập title' onChange={e => setTitle(e.target.value)} />
+            {
+                tabs.map((tab, index) => {
+                    return <button className={tab === type ? 'active-tab' : ''} onClick={() => setType(tab)} key={index}>{tab}</button>
+                })
+            }
+            <ul>
+                {
+                    photos.map((photo, index) => {
+                        return <li key={index}>{photo.title || photo.name}</li>
+                    })
+                }
+            </ul>
         </div>
     )
 } 
